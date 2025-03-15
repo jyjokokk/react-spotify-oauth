bbec924b3bfcb118574998e5f94432f35f8c4be7
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-03-15 20:10:22 +0200

    vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      exclude: [
+        '**/*.spec.tsx',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        '**/*.d.ts',
+        '**/*.config.ts',
+        '**/*.setup.ts',
+        '**/*.type.ts'
+      ]
+    }
   }
 })
